0494a97fc65cbaa3168ea19364694cdddfce83e2
alexwatts83 <alexjwatts83@gmail.com>
2020-05-04 20:35:11 +1000

    initial commit

---
+++ WebAdvert.SearchApi/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAdvert.SearchApi.Models;
+using WebAdvert.SearchApi.Services;
+
+namespace WebAdvert.SearchApi.Controllers
+{
+    public class SearchController : ControllerBase
+    {
+        private readonly ILogger<SearchController> _logger;
+        private readonly ISearchService _searchService;
+
+        public SearchController(ISearchService searchService, ILogger<SearchController> logger)
+        {
+            _searchService = searchService;
+            _logger = logger;
+        }
+
+        public async Task<string> Index()
+        {
+            var now = DateTime.Now;
+            return await Task.FromResult(now.ToString()).ConfigureAwait(false);
+        }
+
+        [HttpGet]
+        [Route("{keyword}")]
+        public async Task<List<AdvertType>> Get(string keyword)
+        {
+            _logger.LogInformation("Search method was called");
+            return await _searchService.Search(keyword).ConfigureAwait(false);
+        }
+    }
+}
---
+++ WebAdvert.SearchApi/Services/ISearchService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+using WebAdvert.SearchApi.Models;
+
+namespace WebAdvert.SearchApi.Services
+{
+    public interface ISearchService
+    {
+        Task<List<AdvertType>> Search(string keyword);
+    }
+
+    public class SearchService : ISearchService
+    {
+        private readonly IElasticClient _client;
+        public SearchService(IElasticClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<AdvertType>> Search(string keyword)
+        {
+            var searchResponse = await _client.SearchAsync<AdvertType>(search => search.
+                Query(query => query.
+                    Term(field => field.Title, keyword.ToLower())
+                )).ConfigureAwait(false);
+
+            return searchResponse.Hits.Select(hit => hit.Source).ToList();
+        }
+    }
+}
